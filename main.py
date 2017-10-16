d44efcbc110810a145b6af619f80007ff617c9ca
Larry Hastings <larry@hastings.org>
2017-10-16 20:05:20 +0100

    Throw away lame Vector2D, use wasabi.geom.vector.

--- main.py
+++ main.py
@@ -11,6 +11,12 @@ import pymunk
 import sys
 # pip3.6 install tmx
 import tmx
+# pip3.6 install wasabi.geom
+from wasabi.geom.vector import Vector, v
+from wasabi.geom.vector import zero as vector_zero
+from wasabi.geom.vector import unit_x as vector_unit_x
+from wasabi.geom.vector import unit_y as vector_unit_y
+
 
 key = pyglet.window.key
 EVENT_HANDLED = pyglet.event.EVENT_HANDLED
@@ -21,93 +27,23 @@ pyglet.resource.path = [".", "gfx/kenney_roguelike/Spritesheet"]
 pyglet.resource.reindex()
 
 
-class Vector2D:
-    def __init__(self, x, y=None):
-        if y is None:
-            assert isinstance(x, Vector2D)
-            self.x = x.x
-            self.y = x.y
-            return
 
-        self.x = x
-        self.y = y
+def vector_clamp(v, other):
+    if other.x == 0:
+        x = 0
+    elif other.x < 0:
+        x = max(v.x, other.x)
+    else:
+        x = min(v.x, other.x)
 
-    def __repr__(self):
-        return f"<{self.__class__.__name__} ({self.x}, {self.y})>"
+    if other.y == 0:
+        y = 0
+    elif other.y < 0:
+        y = max(v.y, other.y)
+    else:
+        y = min(v.y, other.y)
 
-    def __add__(self, other):
-        if isinstance(other, Vector2D):
-            return Vector2D(self.x + other.x, self.y + other.y)
-        return Vector2D(self.x + other, self.y + other)
-
-    def __sub__(self, other):
-        if isinstance(other, Vector2D):
-            return Vector2D(self.x - other.x, self.y - other.y)
-        return Vector2D(self.x - other, self.y - other)
-
-    def __mul__(self, other):
-        if isinstance(other, Vector2D):
-            return Vector2D(self.x * other.x, self.y * other.y)
-        return Vector2D(self.x * other, self.y * other)
-
-    # def __lt__(self, other):
-    #     assert isinstance(other, Vector2D)
-    #     return (self.x < other.x) or (self.y < other.y)
-
-    # def __le__(self, other):
-    #     assert isinstance(other, Vector2D)
-    #     return (self.x <= other.x) and (self.y <= other.y)
-
-    def __eq__(self, other):
-        assert isinstance(other, Vector2D)
-        return (self.x == other.x) and (self.y == other.y)
-
-    # def __ge__(self, other):
-    #     assert isinstance(other, Vector2D)
-    #     return (self.x >= other.x) and (self.y >= other.y)
-
-    # def __gt__(self, other):
-    #     assert isinstance(other, Vector2D)
-    #     return (self.x > other.x) and (self.y > other.y)
-
-    def __ne__(self, other):
-        assert isinstance(other, Vector2D)
-        return (self.x != other.x) or (self.y != other.y)
-
-    def __truediv__(self, other):
-        if isinstance(other, Vector2D):
-            return Vector2D(self.x / other.x, self.y / other.y)
-        return Vector2D(self.x / other, self.y / other)
-
-    def __bool__(self):
-        return bool(self.x or self.y)
-
-    def __getitem__(self, key):
-        assert hasattr(key, '__index__')
-        index = key.__index__()
-        assert key in (0, 1)
-        if key == 0:
-            return self.x
-        else:
-            return self.y
-
-    # TODO
-    # this modifies self instead of returning a new object
-    # inconsistent api here
-    def clamp(self, other):
-        if other.x == 0:
-            self.x = 0
-        elif other.x < 0:
-            self.x = max(self.x, other.x)
-        else:
-            self.x = min(self.x, other.x)
-
-        if other.y == 0:
-            self.y = 0
-        elif other.y < 0:
-            self.y = max(self.y, other.y)
-        else:
-            self.y = min(self.y, other.y)
+    return Vector((x, y))
 
 
 
@@ -134,11 +70,11 @@ class Level:
 
         self.map.set_viewport(0, 0, window.width, window.height)
         self.background_tiles, self.collision_tiles, self.player_position_tiles = (layer.tiles for layer in self.tiles.layers)
-        self.upper_left = Vector2D(0, 0)
-        self.lower_right = Vector2D(
+        self.upper_left = vector_zero
+        self.lower_right = Vector((
             self.tiles.width * self.tiles.tilewidth,
             self.tiles.height * self.tiles.tileheight,
-            )
+            ))
 
         self.foreground_sprite_group = pyglet.graphics.OrderedGroup(self.map.last_group + 1)
 
@@ -169,7 +105,7 @@ class Level:
 
     def position_to_tile_index(self, x, y=None):
         if y is None:
-            assert isinstance(x, Vector2D)
+            assert isinstance(x, Vector)
             y = x.y
             x = x.x
         index = int(((y // self.tiles.tileheight) * (self.tiles.width)) + 
@@ -182,7 +118,7 @@ class Level:
         x = index - (y * self.tiles.width) 
         y *= self.tiles.tileheight
         x *= self.tiles.tilewidth
-        return Vector2D(x, y)
+        return Vector((x, y))
 
     def collision_tile_at(self, x, y=None):
         return self.collision_tiles[self.position_to_tile_index(x, y)].gid
@@ -192,7 +128,7 @@ level = Level("prototype")
 
 
 sin_45degrees = math.sin(math.pi / 4)
-vector_45degrees = Vector2D(sin_45degrees, sin_45degrees)
+vector_45degrees = Vector((sin_45degrees, sin_45degrees))
 
 
 class Player:
@@ -205,23 +141,23 @@ class Player:
                 self.position = level.tile_index_to_position(i)
                 break
         else:
-            self.position = Vector2D(0, 0)
+            self.position = vector_zero
         # adjust player position
         # TODO why is this what we wanted?!
-        self.position.y += level.tiles.tileheight 
+        self.position += Vector((0, level.tiles.tileheight))
 
-        self.desired_speed = Vector2D(0, 0)
-        self.normalized_desired_speed = Vector2D(0, 0)
+        self.desired_speed = vector_zero
+        self.normalized_desired_speed = vector_zero
         self.speed_multiplier = 10
-        self.speed = Vector2D(0, 0)
+        self.speed = vector_zero
         self.acceleration_frames = 20 # 20/60 of a second to get to full speed
         self.movement_keys = set()
         # coordinate system has (0, 0) at upper left
         self.movement_vectors = {
-            key.UP:    Vector2D(0, -1),
-            key.DOWN:  Vector2D(0, 1),
-            key.LEFT:  Vector2D(-1, 0),
-            key.RIGHT: Vector2D(1, 0),
+            key.UP:    vector_unit_y * -1,
+            key.DOWN:  vector_unit_y,
+            key.LEFT:  vector_unit_x * -1,
+            key.RIGHT: vector_unit_x,
             }
         self.movement_opposites = {
             key.UP: key.DOWN,
@@ -237,7 +173,7 @@ class Player:
         if not (self.desired_speed.x and self.desired_speed.y):
             self.normalized_desired_speed = self.desired_speed
         else:
-            self.normalized_desired_speed = self.desired_speed * vector_45degrees
+            self.normalized_desired_speed = Vector((self.desired_speed.x * vector_45degrees.x, self.desired_speed.y * vector_45degrees.y))
         self.normalized_desired_speed *= self.speed_multiplier
         # print("normalized desired speed is now", self.normalized_desired_speed)
 
@@ -291,18 +227,19 @@ class Player:
         if self.speed != self.normalized_desired_speed:
             delta = self.normalized_desired_speed / self.acceleration_frames
             self.speed += delta
-            self.speed.clamp(self.normalized_desired_speed)
+            self.speed = vector_clamp(self.speed, self.normalized_desired_speed)
             # print("SPEED IS NOW", self.speed)
         if self.speed:
             new_position = self.position + self.speed
-            if new_position.x < level.upper_left.x:
-                new_position.x = level.upper_left.x
-            if new_position.y < level.upper_left.y:
-                new_position.y = level.upper_left.y
-            if new_position.x > level.lower_right.x:
-                new_position.x = level.lower_right.x
-            if new_position.y > level.lower_right.y:
-                new_position.y = level.lower_right.y
+            x, y = new_position
+            if x < level.upper_left.x:
+                x = level.upper_left.x
+            if y < level.upper_left.y:
+                y = level.upper_left.y
+            if x > level.lower_right.x:
+                x = level.lower_right.x
+            if y > level.lower_right.y:
+                y = level.lower_right.y
             # now check if we're colliding
             for key in self.movement_keys:
                 vector = self.movement_vectors[key] * 8
@@ -310,10 +247,11 @@ class Player:
                 if level.collision_tile_at(tile_position):
                     # throw away movement in bad direction
                     if vector.x:
-                        new_position.x = self.position.x
+                        x = self.position.x
                     else:
-                        new_position.y = self.position.y
+                        y = self.position.y
             # print("new position", new_position)
+            new_position = Vector((x, y))
             if self.position != new_position:
                 self.position = new_position
                 # print("position now", self.position)
