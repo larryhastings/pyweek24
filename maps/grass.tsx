895b3e8ab21e0ee66909cd0c06a9d96d51af8ff8
Larry Hastings <larry@hastings.org>
2017-10-21 19:34:14 +0100

    Rename levels.

--- maps/grass.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Grass" tilewidth="32" tileheight="32" tilecount="4" columns="2">
- <image source="gfx/grass.png" width="64" height="64"/>
-</tileset>
